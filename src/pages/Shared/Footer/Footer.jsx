4c858c58a4a879be6cce082457d2dfdd3d136711
shuvo9620 <smahfuzur0@gmail.com>
2023-05-02 13:24:37 +0600

    routes & shared components added

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-center'><small>copy right by shuvo</small></p>
+        </div>
+    );
+};
+
+export default Footer;
