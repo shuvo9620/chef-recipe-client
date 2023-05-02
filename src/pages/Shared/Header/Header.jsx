eb8cf943c29a90bd5adacaaf1c60fab1fc1ff36c
shuvo9620 <smahfuzur0@gmail.com>
2023-05-02 14:49:40 +0600

    nav bar added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Navbar, Nav, NavDropdown, Button, Image, Container } from 'react-bootstrap';
+import profile_img from '../../../assets/images/profile.png'
 
 const Header = () => {
+    const signedIn = true;
+    const userName = "Shuvo";
     return (
-        <div>
-            <h3>this is header</h3>
-        </div>
+        <Container>
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand href="#">Chef Recipe</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto d-flex">
+                            <Nav.Link href="#">Home</Nav.Link>
+                            <Nav.Link href="#">Blog</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            {signedIn ?
+                                <Image style={{ height: '40px' }} src={profile_img} roundedCircle title={userName} className="flex-grow-1" />
+                                :
+                                <Button variant="outline-success">Login</Button>
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+        </Container>
     );
 };
 
